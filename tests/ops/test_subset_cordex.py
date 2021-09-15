a69e4ac879b82d35ca16c313655bb37f60d46682
Pingu Carsti <ehbrecht@dkrz.de>
2021-09-15 18:05:01 +0200

    added subset cordex tests

---
+++ tests/ops/test_subset_cordex.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from clisops.ops.subset import subset
+
+from .._common import (
+    C3S_CORDEX_PSL,
+)
+
+def _check_output_nc(result, fname="output_001.nc"):
+    assert fname in [Path(_).name for _ in result]
+
+
+def test_subset_time_cordex(load_esgf_test_data, tmpdir):
+    """Test subset on cordex data"""
+
+    result = subset(
+        ds=C3S_CORDEX_PSL,
+        time=("2010-01-01", "2011-12-31"),
+        # area=(-5.25, 42.39, 8.05, 50.86),  # France
+        output_dir=tmpdir,
+        output_type="netcdf",
+        file_namer="standard",
+    )
+
+    _check_output_nc(
+        result,
+        fname="psl_EUR-11_MOHC-HadGEM2-ES_rcp85_r1i1p1_IPSL-WRF381P_v1_day_20100101-20111230.nc",
+    )
